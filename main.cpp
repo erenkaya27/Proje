282cd6126e7b8e70d9027ce513d468003c716a3b
erenkaya27 <abdulaherenkaya35@gmail.com>
2024-03-07 13:06:47 +0300

    main.cppye kodlar eklendi
    Şifreleme ve deşifreleme fonksiyonları eklendi

--- main.cpp
+++ main.cpp
@@ -1,13 +1,39 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
-int main()
-{
 
-cout<<"Helllo WORLD"<<endl;
-return 0;
-
-
-
-
-
+string sifrele( string metin, string anahtar) {
+    string sifreliMetin = metin;
+    for (int i = 0; i < metin.length(); ++i) {
+        sifreliMetin[i] = metin[i] ^ anahtar[i % anahtar.length()];
+    }
+    return sifreliMetin;
 }
+
+string coz( string sifreliMetin,  string anahtar) {
+    return sifrele(sifreliMetin, anahtar);  
+}
+
+int main() {
+    string metin, anahtar;
+
+    
+    cout << "Şifrelenecek metni girin: ";
+    cin>>metin;
+
+    cout << "Anahtar metni girin: ";
+    cin>>anahtar;
+
+    
+    string sifreliMetin = sifrele(metin, anahtar);
+    cout << "Şifrelenmiş metin: " << sifreliMetin << endl;
+
+    
+    string cozulmusMetin = coz(sifreliMetin, anahtar);
+    cout << "Deşifrelenmiş metin: " << cozulmusMetin << endl;
+
+    return 0;
+}
+
+
